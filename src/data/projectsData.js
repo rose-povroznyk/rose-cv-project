6bbc0af72199162db9e909f60edd6c57b9a48827
Rose <pas.une.mowgli@gmail.com>
2024-08-07 21:56:12 +0300

    Add new links to projects

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,5 +1,9 @@
 export const projectsDataEng = [
-  ['#', 'Hogwarts-form-project', 'HTML, CSS'],
+  [
+    'https://pas-une-mowgli.github.io/form-practice/',
+    'Hogwarts-form-project',
+    'HTML, CSS',
+  ],
   [
     'https://pas-une-mowgli.github.io/pulse-project/dist/',
     'Pulse-project',
@@ -16,28 +20,52 @@ export const projectsDataEng = [
     'HTML, CSS | + CSS Grid Layout',
   ],
   [
-    'src/projects/explore-the-world-project',
+    'https://pas-une-mowgli.github.io/explore-the-world-project/',
     'Explore-the-world-project',
     'HTML, CSS',
   ],
-  ['#', 'Fahrenheit-to-Celsius-project', 'HTML, CSS, Js'],
-  ['#', 'FizzBuzz-project', 'HTML, CSS, JS'],
-  ['#', 'ToDo-project', 'HTML, CSS, JS'],
-  ['#', 'Matrix-project', 'HTML, Sass, JS, React'],
-  ['#', 'UDK-project', 'HTML, Sass, JS, React'],
+  [
+    'https://pas-une-mowgli.github.io/fahrenheit-to-celsius-project/',
+    'Fahrenheit-to-Celsius-project',
+    'HTML, CSS, Js',
+  ],
+  [
+    'https://pas-une-mowgli.github.io/fizzbuzz-project/',
+    'FizzBuzz-project',
+    'HTML, CSS, JS',
+  ],
+  [
+    'https://pas-une-mowgli.github.io/todo-project-js/',
+    'ToDo-project',
+    'HTML, CSS, JS',
+  ],
+  [
+    'https://matrix-organizer.netlify.app/',
+    'Matrix-project',
+    'HTML, Sass, JS, React',
+  ],
+  [
+    'https://udk-bbk-convector-online.netlify.app/',
+    'UDK-project',
+    'HTML, Sass, JS, React',
+  ],
   [
     'https://the-lady-or-the-tiger-game.netlify.app/',
     'The-lady-and-the-tiger-project',
     'HTML, Sass, JS, React | + useReducer + and thanks for the inspiration, Raymond Smullyan',
   ],
   [
-    '#',
+    'https://github.com/rose-povroznyk/parking-inspection',
     'Parking-inspection-project',
     'HTML, Sass, JS, React, Redux, PostgresSQL, MongoDB  | still in process … thanks for this project , mentor Roman',
   ],
 ];
 export const projectsDataUa = [
-  ['#', 'Hogwarts-form-project', 'HTML, CSS'],
+  [
+    'https://pas-une-mowgli.github.io/form-practice/',
+    'Hogwarts-form-project',
+    'HTML, CSS',
+  ],
   [
     'https://pas-une-mowgli.github.io/pulse-project/dist/',
     'Pulse-project',
@@ -53,19 +81,43 @@ export const projectsDataUa = [
     'Store-game-project',
     'HTML, CSS | + CSS Grid Layout',
   ],
-  ['#', 'Explore-the-world-project', 'HTML, CSS'],
-  ['#', 'Fahrenheit-to-Celsius-project', 'HTML, CSS, Js'],
-  ['#', 'FizzBuzz-project', 'HTML, CSS, JS'],
-  ['#', 'ToDo-project', 'HTML, CSS, JS'],
-  ['#', 'Matrix-project', 'HTML, Sass, JS, React'],
-  ['#', 'UDK-project', 'HTML, Sass, JS, React'],
   [
-    '#',
+    'https://pas-une-mowgli.github.io/explore-the-world-project/',
+    'Explore-the-world-project',
+    'HTML, CSS',
+  ],
+  [
+    'https://pas-une-mowgli.github.io/fahrenheit-to-celsius-project/',
+    'Fahrenheit-to-Celsius-project',
+    'HTML, CSS, Js',
+  ],
+  [
+    'https://pas-une-mowgli.github.io/fizzbuzz-project/',
+    'FizzBuzz-project',
+    'HTML, CSS, JS',
+  ],
+  [
+    'https://pas-une-mowgli.github.io/todo-project-js/',
+    'ToDo-project',
+    'HTML, CSS, JS',
+  ],
+  [
+    'https://matrix-organizer.netlify.app/',
+    'Matrix-project',
+    'HTML, Sass, JS, React',
+  ],
+  [
+    'https://udk-bbk-convector-online.netlify.app/',
+    'UDK-project',
+    'HTML, Sass, JS, React',
+  ],
+  [
+    'https://the-lady-or-the-tiger-game.netlify.app/',
     'The-lady-and-the-tiger-project',
     'HTML, Sass, JS, React | + useReducer + дякую за натхнення Реймонду Смалліану',
   ],
   [
-    '#',
+    'https://github.com/rose-povroznyk/parking-inspection',
     'Parking-inspection-project',
     'HTML, Sass, JS, React, Redux, PostgresSQL, MongoDB | в процесі… + дякую за цей проект ментору Роману',
   ],
