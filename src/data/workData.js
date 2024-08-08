12ec49c5c949bbfb2dc38204f783e405562c4638
Rose <pas.une.mowgli@gmail.com>
2024-08-08 08:27:30 +0300

    Change worData

--- src/data/workData.js
+++ src/data/workData.js
@@ -1,7 +1,7 @@
 export const workDataEng = [
   [' Full Stack JS Developer', '2024 | Ukraine', 'Loading ... :)'],
   [
-    'Librarian | School #5',
+    'Librarian/Polish language teacher | School #5',
     'January 2013 - up to now | Ternopil',
     'Yes, this job is not related to programming at all, but working at school taught me, how to manage my time, work on multiple projects together, communicate with people, resolve conflicts, constantly think about various things, and most importantly, learn how to learn!',
   ],
@@ -10,7 +10,7 @@ export const workDataEng = [
 export const workDataUa = [
   [' Full Stack JS Developer', '2024 | Україна', 'Завантаження ... :)'],
   [
-    'Бібліотекар | Школа #5',
+    'Бібліотекар/вчитель польської мови | Школа #5',
     'Січень 2013 - до цього часу | Тернопіль',
     "Так, це робота зовсім не пов'язана з програмуванням, тим не менше саме працюючи в школі я навчилася організовувати свій час, працювати над багатьма проектами одночасно, багато комунікувати з людьми, вміти вирішувати конфлікти, постійно над чимось роздумувати і найголовніше - я навчилася вчитися!",
   ],
